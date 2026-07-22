539c47cf0eb5fa34f4f721758bce8c9dfc5a6666
MuhammadAli2211 <moonansari936@gmail.com>
2026-07-22 16:48:09 +0500

    Remove /api from backend URL

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
-    "https://blog-backend-bay-pi.vercel.app/api",
+    "https://blog-backend-bay-pi.vercel.app",
 });
 
 API.interceptors.request.use((config) => {
